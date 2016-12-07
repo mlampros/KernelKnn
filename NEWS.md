8b1eda4ccc87a32993beb80a0b5f59ba464be768
mlampros <mouselimislampros@hotmail.com>
2016-12-07 20:17:30 +0200

    re-implementation of rcpp file due to ASAN-memory-errors

--- NEWS.md
+++ NEWS.md
@@ -1,12 +1,17 @@
-## OpenImageR 1.0.2
+
+## KernelKnn 1.0.3
+
+reimplementation of the Rcpp function due to ASAN-memory-errors
+
+## KernelKnn 1.0.2
 
 I updated the Description file with a URL and a BugReports web-address.
 
-## OpenImageR 1.0.1
+## KernelKnn 1.0.1
 
 Currently, Software platforms like OSX do not support openMP, thus I've made openMP optional for all cpp functions.
 
-## OpenImageR 1.0.0
+## KernelKnn 1.0.0
 
 
 
--- src/distance_metrics.cpp
+++ src/distance_metrics.cpp
@@ -1,9 +1,9 @@
-//========================================================================================================================================================================================
 
 # include <RcppArmadillo.h>
 // [[Rcpp::depends("RcppArmadillo")]]
 // [[Rcpp::plugins(openmp)]]
 // [[Rcpp::plugins(cpp11)]]
+
 
 #ifdef _OPENMP
 #include <omp.h>
@@ -11,105 +11,116 @@
 
 
 
-// [[Rcpp::export]]
-Rcpp::List knn_index_dist_rcpp(arma::mat& MATRIX, arma::mat& TEST_DATA, int k, std::string method, int threads, double eps = 1.0e-6) {
+// return data struct
+//
 
-  #ifdef _OPENMP
-  omp_set_num_threads(threads);
-  #endif
+struct return_data {
+  
+  arma::mat knn_idx;
+  
+  arma::mat knn_dist;
+};
 
-  if (TEST_DATA.is_empty()) {
 
-    arma::mat cov_mat;
 
-    if (method == "mahalanobis") {
+// kernelknn-class
+//
 
-      cov_mat = arma::inv(arma::cov(MATRIX));
-    }
 
-    int ITERS = MATRIX.n_rows;
-
-    arma::mat out(ITERS, k, arma::fill::zeros);                                                           // loop to calculate the distances for the TRAIN data [MATRIX]
-
-    arma::mat sorted_train_dist(ITERS, k, arma::fill::zeros);
+class kernelKnn {
+  
+  private:
     
-    #ifdef _OPENMP
-    #pragma omp parallel for schedule(static)
-    #endif
-    for (int i = 0; i < ITERS; i++) {
-
+    arma::mat knn_indices;
+    
+    arma::mat knn_distances;
+    
+  public:
+    
+    kernelKnn() { }
+    
+    
+    // inner loop
+    //
+    
+    arma::rowvec inner_loop(arma::mat& MATRIX_1st, arma::mat& MATRIX_2nd, int i, int ITERS, int k, std::string method, int threads, arma::mat cov_mat, double eps = 1.0e-6) {
+      
+      #ifdef _OPENMP
+      omp_set_num_threads(threads);
+      #endif
+      
       double tmp_idx;
       
       arma::rowvec tmp_out = arma::zeros<arma::rowvec>(ITERS);
-
+      
       #ifdef _OPENMP
       #pragma omp parallel for schedule(static)
       #endif
       for (int j = 0; j < ITERS; j++) {                                                                       // http://scikit-learn.org/stable/modules/generated/sklearn.neighbors.DistanceMetric.html
-
+        
         if (method == "euclidean") {
-
-          tmp_idx = std::sqrt(arma::as_scalar(arma::accu(arma::square((MATRIX.row(j) - MATRIX.row(i))))));
+          
+          tmp_idx = std::sqrt(arma::as_scalar(arma::accu(arma::square((MATRIX_1st.row(j) - MATRIX_2nd.row(i))))));
         }
-
+        
         else if (method == "manhattan") {
-
-          tmp_idx = arma::as_scalar(arma::accu(arma::abs((MATRIX.row(j) - MATRIX.row(i)))));
+          
+          tmp_idx = arma::as_scalar(arma::accu(arma::abs((MATRIX_1st.row(j) - MATRIX_2nd.row(i)))));
         }
-
+        
         else if (method == "chebyshev") {
-
-          tmp_idx = arma::as_scalar(max(arma::abs((MATRIX.row(j) - MATRIX.row(i)))));
+          
+          tmp_idx = arma::as_scalar(max(arma::abs((MATRIX_1st.row(j) - MATRIX_2nd.row(i)))));
         }
-
+        
         else if (method == "canberra") {
-
-          tmp_idx = arma::as_scalar(arma::accu(arma::abs((MATRIX.row(j) - MATRIX.row(i)) + eps)/(arma::abs(MATRIX.row(j)) + arma::abs(MATRIX.row(i)) + eps)));                 // added 1.0e-6 otherwise rstudio crashes
+          
+          tmp_idx = arma::as_scalar(arma::accu(arma::abs((MATRIX_1st.row(j) - MATRIX_2nd.row(i)) + eps)/(arma::abs(MATRIX_1st.row(j)) + arma::abs(MATRIX_2nd.row(i)) + eps)));                 // added 1.0e-6 otherwise rstudio crashes
         }
-
+        
         else if (method == "braycurtis") {
-
-          tmp_idx = arma::as_scalar(arma::accu(arma::abs((MATRIX.row(j) - MATRIX.row(i))))/(arma::accu(arma::abs(MATRIX.row(j))) + arma::accu(arma::abs(MATRIX.row(i)))));
+          
+          tmp_idx = arma::as_scalar(arma::accu(arma::abs((MATRIX_1st.row(j) - MATRIX_2nd.row(i))))/(arma::accu(arma::abs(MATRIX_1st.row(j))) + arma::accu(arma::abs(MATRIX_2nd.row(i)))));
         }
-
+        
         else if (method == "pearson_correlation") {
-
-          tmp_idx = arma::as_scalar(1.0 - arma::cor(MATRIX.row(j), MATRIX.row(i)));
+          
+          tmp_idx = arma::as_scalar(1.0 - arma::cor(MATRIX_1st.row(j), MATRIX_2nd.row(i)));
         }
-
+        
         else if (method == "simple_matching_coefficient") {
           
           double a = eps;
           double d = eps;
           
-          for (int t = 0; t < MATRIX.row(j).n_elem; t++) {
+          for (unsigned int t = 0; t < MATRIX_1st.row(j).n_elem; t++) {
             
-            if (MATRIX.row(j)(t) == 1 && MATRIX.row(i)(t) == 1) {
+            if (MATRIX_1st.row(j)(t) == 1 && MATRIX_2nd.row(i)(t) == 1) {
               
               a += 1.0;}
             
-            if (MATRIX.row(j)(t) == 0 && MATRIX.row(i)(t) == 0) {
+            if (MATRIX_1st.row(j)(t) == 0 && MATRIX_2nd.row(i)(t) == 0) {
               
               d += 1.0;
             }
           }
           
-          tmp_idx = 1.0 - ((a + d) / MATRIX.row(j).n_elem);
+          tmp_idx = 1.0 - ((a + d) / MATRIX_1st.row(j).n_elem);
         }
-
+        
         else if (method == "minkowski") {                                                                                     // by default the order of the minkowski parameter equals k
-
-          tmp_idx = std::pow(arma::as_scalar(arma::accu(arma::pow(arma::abs((MATRIX.row(j) - MATRIX.row(i))), k))), 1.0/k);
+          
+          tmp_idx = std::pow(arma::as_scalar(arma::accu(arma::pow(arma::abs((MATRIX_1st.row(j) - MATRIX_2nd.row(i))), k))), 1.0/k);
         }
-
+        
         else if (method == "hamming") {                                                                                     // for binary data
-
-          tmp_idx = arma::as_scalar(accu(MATRIX.row(j) != MATRIX.row(i))/(MATRIX.row(j).n_elem * 1.0));
+          
+          tmp_idx = arma::as_scalar(accu(MATRIX_1st.row(j) != MATRIX_2nd.row(i))/(MATRIX_1st.row(j).n_elem * 1.0));
         }
-
+        
         else if (method == "mahalanobis") {                                                                                     // first create covariance matrix from data
-
-          tmp_idx = arma::as_scalar(std::sqrt(arma::as_scalar(((MATRIX.row(j) - MATRIX.row(i)) * cov_mat) * (MATRIX.row(j) - MATRIX.row(i)).t())));
+          
+          tmp_idx = arma::as_scalar(std::sqrt(arma::as_scalar(((MATRIX_1st.row(j) - MATRIX_2nd.row(i)) * cov_mat) * (MATRIX_1st.row(j) - MATRIX_2nd.row(i)).t())));
         }
         
         else if (method == "jaccard_coefficient") {                                                                                     // for binary data
@@ -118,17 +129,17 @@ Rcpp::List knn_index_dist_rcpp(arma::mat& MATRIX, arma::mat& TEST_DATA, int k, s
           double b = eps;
           double c = eps;
           
-          for (int t = 0; t < MATRIX.row(j).n_elem; t++) {
+          for (unsigned int t = 0; t < MATRIX_1st.row(j).n_elem; t++) {
             
-            if (MATRIX.row(j)(t) == 1 && MATRIX.row(i)(t) == 1) {
+            if (MATRIX_1st.row(j)(t) == 1 && MATRIX_2nd.row(i)(t) == 1) {
               
               a += 1.0;}
             
-            if (MATRIX.row(j)(t) == 1 && MATRIX.row(i)(t) == 0) {
+            if (MATRIX_1st.row(j)(t) == 1 && MATRIX_2nd.row(i)(t) == 0) {
               
               b += 1.0;}
             
-            if (MATRIX.row(j)(t) == 0 && MATRIX.row(i)(t) == 1) {
+            if (MATRIX_1st.row(j)(t) == 0 && MATRIX_2nd.row(i)(t) == 1) {
               
               c += 1.0;
             }
@@ -141,15 +152,15 @@ Rcpp::List knn_index_dist_rcpp(arma::mat& MATRIX, arma::mat& TEST_DATA, int k, s
           
           double a = eps;
           
-          for (int t = 0; t < MATRIX.row(j).n_elem; t++) {
+          for (unsigned int t = 0; t < MATRIX_1st.row(j).n_elem; t++) {
             
-            if (MATRIX.row(j)(t) == 1 && MATRIX.row(i)(t) == 1) {
+            if (MATRIX_1st.row(j)(t) == 1 && MATRIX_2nd.row(i)(t) == 1) {
               
               a += 1.0;
             }
           }
           
-          tmp_idx = 1.0 - (a / MATRIX.row(j).n_elem);
+          tmp_idx = 1.0 - (a / MATRIX_1st.row(j).n_elem);
         }
         
         else {
@@ -166,182 +177,137 @@ Rcpp::List knn_index_dist_rcpp(arma::mat& MATRIX, arma::mat& TEST_DATA, int k, s
           tmp_out(j) = tmp_idx;
         }
       }
-
-      arma::rowvec index_out = arma::conv_to< arma::rowvec >::from(sort_index(tmp_out, "ascend")) + 1;
-
-      out.row(i) = index_out.subvec(1,k);
-
-      arma::rowvec tmp_sort = arma::conv_to< arma::rowvec >::from(arma::sort(tmp_out, "ascend"));
-
-      sorted_train_dist.row(i) = tmp_sort.subvec(1,k);
-    }
-
-    return(Rcpp::List::create(Rcpp::Named("train_knn_idx") = out, Rcpp::Named("train_knn_dist") = sorted_train_dist));
-  }
-
-  if (!TEST_DATA.is_empty()) {                                                                                  // check if object is NOT empty OTHERWISE return only the train data
-
-    arma::mat cov_mat;
-
-    if (method == "mahalanobis") {
-
-      cov_mat = arma::inv(arma::cov(arma::join_vert(MATRIX, TEST_DATA)));
-    }
-
-    int ITERS_TEST = TEST_DATA.n_rows;
-
-    int ITERS_TRAIN = MATRIX.n_rows;
-
-    arma::mat TEST_OUT(ITERS_TEST, k, arma::fill::zeros);
-
-    arma::mat sorted_dist(ITERS_TEST, k, arma::fill::zeros);
-
-    #ifdef _OPENMP
-    #pragma omp parallel for schedule(static)
-    #endif
-    for (int i = 0; i < ITERS_TEST; i++) {
       
-      double tmp_idx;
-
-      arma::rowvec tmp_out = arma::zeros<arma::rowvec>(ITERS_TRAIN);
-
+      return tmp_out;
+    }
+    
+    
+    // train-data-input-only
+    //
+    
+    void train_only(arma::mat& MATRIX, int k, std::string method, int threads, double eps = 1.0e-6) {
+      
+      arma::mat cov_mat;
+      
+      if (method == "mahalanobis") {
+        
+        cov_mat = arma::inv(arma::cov(MATRIX));
+      }
+      
+      int ITERS = MATRIX.n_rows;
+      
+      knn_indices.set_size(ITERS, k);                                                           // loop to calculate the distances for the TRAIN data [MATRIX]
+      
+      knn_distances.set_size(ITERS, k);
+      
       #ifdef _OPENMP
       #pragma omp parallel for schedule(static)
       #endif
-      for (int j = 0; j < ITERS_TRAIN; j++) {
-
-        if (method == "euclidean") {
-
-          tmp_idx = std::sqrt(arma::as_scalar(arma::accu(arma::square((MATRIX.row(j) - TEST_DATA.row(i))))));}
-
-        else if (method == "manhattan") {
-
-          tmp_idx = arma::as_scalar(arma::accu(arma::abs((MATRIX.row(j) - TEST_DATA.row(i)))));
-        }
-
-        else if (method == "chebyshev") {
-
-          tmp_idx = arma::as_scalar(arma::max(arma::abs((MATRIX.row(j) - TEST_DATA.row(i)))));
-        }
-
-        else if (method == "canberra") {
-
-          tmp_idx = arma::as_scalar(arma::accu(arma::abs((MATRIX.row(j) - TEST_DATA.row(i)) + eps)/(arma::abs(MATRIX.row(j)) + arma::abs(TEST_DATA.row(i)) + eps)));         // added 1.0e-6 otherwise rstudio crashes
-        }
-
-        else if (method == "braycurtis") {
-
-          tmp_idx = arma::as_scalar(arma::accu(arma::abs((MATRIX.row(j) - TEST_DATA.row(i))))/(arma::accu(arma::abs(MATRIX.row(j))) + arma::accu(arma::abs(TEST_DATA.row(i)))));
-        }
-
-        else if (method == "pearson_correlation") {
-
-          tmp_idx = arma::as_scalar(1.0 - arma::cor(MATRIX.row(j), TEST_DATA.row(i)));
-        }
-
-        else if (method == "simple_matching_coefficient") {                                                                                     // for binary data
-          
-          double a = eps;
-          double d = eps;
-          
-          for (int t = 0; t < MATRIX.row(j).n_elem; t++) {
-            
-            if (MATRIX.row(j)(t) == 1 && TEST_DATA.row(i)(t) == 1) {
-              
-              a += 1.0;}
-            
-            if (MATRIX.row(j)(t) == 0 && TEST_DATA.row(i)(t) == 0) {
-              
-              d += 1.0;
-            }
-          }
-          
-          tmp_idx = 1.0 - ((a + d) / MATRIX.row(j).n_elem);
-        }
-
-        else if (method == "minkowski") {                                                                                     //  by default the order of the minkowski parameter equals k
-
-          tmp_idx = std::pow(arma::as_scalar(arma::accu(arma::pow(arma::abs((MATRIX.row(j) - TEST_DATA.row(i))), k))), 1.0/k);
-        }
-
-        else if (method == "hamming") {                                                                                     // for binary data
-
-          tmp_idx = arma::as_scalar(arma::accu(MATRIX.row(j) != TEST_DATA.row(i))/(MATRIX.row(j).n_elem * 1.0));
-        }
-
-        else if (method == "mahalanobis") {                                                                                     //  first create covariance matrix from data 
-
-          tmp_idx = arma::as_scalar(std::sqrt(arma::as_scalar(((MATRIX.row(j) - TEST_DATA.row(i)) * cov_mat) * (MATRIX.row(j) - TEST_DATA.row(i)).t())));
-        }
+      for (int i = 0; i < ITERS; i++) {
         
-        else if (method == "jaccard_coefficient") {                                                                                     // for binary data
-          
-          double a = eps;
-          double b = eps;
-          double c = eps;
-          
-          for (int t = 0; t < MATRIX.row(j).n_elem; t++) {
-            
-            if (MATRIX.row(j)(t) == 1 && TEST_DATA.row(i)(t) == 1) {
-              
-              a += 1.0;}
-            
-            if (MATRIX.row(j)(t) == 1 && TEST_DATA.row(i)(t) == 0) {
-              
-              b += 1.0;}
-            
-            if (MATRIX.row(j)(t) == 0 && TEST_DATA.row(i)(t) == 1) {
-              
-              c += 1.0;
-            }
-          }
-          
-          tmp_idx = 1.0 - (a / (a + b + c));
-        }
+        arma::rowvec tmp_out = inner_loop(MATRIX, MATRIX, i, ITERS, k, method, threads, cov_mat, eps);
         
-        else if (method == "Rao_coefficient") {                                                                                     // for binary data
-          
-          double a = eps;
-          
-          for (int t = 0; t < MATRIX.row(j).n_elem; t++) {
-            
-            if (MATRIX.row(j)(t) == 1 && TEST_DATA.row(i)(t) == 1) {
-              
-              a += 1.0;
-            }
-          }
-          
-          tmp_idx = 1.0 - (a / MATRIX.row(j).n_elem);
-        }
+        arma::rowvec index_out = arma::conv_to< arma::rowvec >::from(arma::sort_index(tmp_out, "ascend")) + 1;
         
-        else {
-          
-          tmp_idx = 0;                                             // default = 0; create exceptions in R, so that tmp_idx is never 0;
-        }
+        knn_indices.row(i) = index_out.subvec(1,k);
         
-        if( tmp_idx != tmp_idx ) { 
-          
-          tmp_out(j) = 1.0;}
+        arma::rowvec tmp_sort = arma::conv_to< arma::rowvec >::from(arma::sort(tmp_out, "ascend"));
         
-        else {
-          
-          tmp_out(j) = tmp_idx;
-        }
+        knn_distances.row(i) = tmp_sort.subvec(1,k);
       }
-
-      arma::rowvec index_out = arma::conv_to< arma::rowvec >::from(arma::sort_index(tmp_out, "ascend")) + 1;
-
-      TEST_OUT.row(i) = index_out.subvec(0,k-1);
-
-      arma::rowvec tmp_sort = arma::conv_to< arma::rowvec >::from(arma::sort(tmp_out, "ascend"));
-
-      sorted_dist.row(i) = tmp_sort.subvec(0,k-1);
     }
+    
+    
+    
+    // test-data-only
+    //
+    
+    void test_only(arma::mat& MATRIX, arma::mat& TEST_DATA, int k, std::string method, int threads, double eps = 1.0e-6) {
+      
+      arma::mat cov_mat;
+      
+      if (method == "mahalanobis") {
+        
+        cov_mat = arma::inv(arma::cov(arma::join_vert(MATRIX, TEST_DATA)));
+      }
+      
+      int ITERS_TEST = TEST_DATA.n_rows;
+      
+      int ITERS_TRAIN = MATRIX.n_rows;
+      
+      knn_indices.set_size(ITERS_TEST, k);
+      
+      knn_distances.set_size(ITERS_TEST, k);
+      
+      #ifdef _OPENMP
+      #pragma omp parallel for schedule(static)
+      #endif
+      for (int i = 0; i < ITERS_TEST; i++) {
+        
+        arma::rowvec tmp_out = inner_loop(MATRIX, TEST_DATA, i, ITERS_TRAIN, k, method, threads, cov_mat, eps);
+        
+        arma::rowvec index_out = arma::conv_to< arma::rowvec >::from(arma::sort_index(tmp_out, "ascend")) + 1;
+        
+        knn_indices.row(i) = index_out.subvec(0,k-1);
+        
+        arma::rowvec tmp_sort = arma::conv_to< arma::rowvec >::from(arma::sort(tmp_out, "ascend"));
+        
+        knn_distances.row(i) = tmp_sort.subvec(0,k-1);
+      }
+    }
+    
+    
+    // return data for either train or test
+    //
+    
+    return_data return_train_test() {
+      
+      return { knn_indices, knn_distances };
+    }
+    
+    
+    ~kernelKnn() { }
+};
 
-    return(Rcpp::List::create(Rcpp::Named("test_knn_idx") = TEST_OUT, Rcpp::Named("test_knn_dist") = sorted_dist));
+
+
+
+//------------------------------------------
+
+
+// function to be exported
+//
+
+// [[Rcpp::export]]
+Rcpp::List knn_index_dist_rcpp(arma::mat& MATRIX, arma::mat& TEST_DATA, int k, std::string method, int threads, double eps = 1.0e-6) {
+  
+  kernelKnn kn;
+  
+  std::string name_idx;
+  
+  std::string name_dist;
+  
+  if (TEST_DATA.is_empty()) {
+    
+    kn.train_only( MATRIX, k, method, threads, eps );
+    
+    name_idx = "train_knn_idx";
+    
+    name_dist = "train_knn_dist";
   }
   
-  return 0;
+  if (!TEST_DATA.is_empty()) {
+    
+    kn.test_only( MATRIX, TEST_DATA, k, method, threads, eps );
+    
+    name_idx = "test_knn_idx";
+    
+    name_dist = "test_knn_dist";
+  }
+  
+  return_data dat = kn.return_train_test();
+  
+  return Rcpp::List::create(Rcpp::Named(name_idx) = dat.knn_idx, Rcpp::Named(name_dist) = dat.knn_dist);
 }
 
-//========================================================================================================================================================================================
+
